c31f1498cd0b54dfe40057831d14397861f2dd86
somrad <somrad.sharma@student.ehb.be>
2023-05-18 12:40:03 +0200

    added email already taken function

--- frontend/components/SignUpScreens/Email.js
+++ frontend/components/SignUpScreens/Email.js
@@ -12,14 +12,34 @@ const EmailPage = ({ route, navigation }) => {
   const { firstName, lastName } = route.params;
   const [email, setEmail] = useState("");
 
-  const handleNext = () => {
+  const checkEmailExists = async (email) => {
+    const usersRef = firebase.database().ref("users");
+    let emailExists = false;
+
+    await usersRef.once("value", (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        if (childSnapshot.val().email === email) {
+          emailExists = true;
+        }
+      });
+    });
+
+    return emailExists;
+  };
+
+  const handleNext = async () => {
     const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     if (email && emailRegex.test(email)) {
-      navigation.navigate("RoleAndDob", {
-        firstName: route.params.firstName,
-        lastName: route.params.lastName,
-        email,
-      });
+      const emailExists = await checkEmailExists(email);
+      if (!emailExists) {
+        navigation.navigate("RoleAndDob", {
+          firstName: route.params.firstName,
+          lastName: route.params.lastName,
+          email,
+        });
+      } else {
+        alert("This email is already taken, please enter another one");
+      }
     } else {
       alert("Please enter a valid email address");
     }
