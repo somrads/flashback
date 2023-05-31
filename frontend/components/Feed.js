80c0ae0c20842b4c0bb11c78fb872d6904ca18e5
somrad <somrad.sharma@student.ehb.be>
2023-05-31 15:35:38 +0200

    camera taking code done

--- frontend/components/Feed.js
+++ frontend/components/Feed.js
@@ -1,10 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Modal,
+} from "react-native";
 import { auth, database } from "../db/firebase";
 import { ref, onValue } from "firebase/database";
 import { COLORS } from "../constants/colors";
 import Add from "../assets/icons/addIcon.svg";
 import tinycolor from "tinycolor2";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import Icon from "react-native-vector-icons/Ionicons";
 
 function darkenColor(color) {
   let colorObj = tinycolor(color);
@@ -19,6 +29,19 @@ function darkenColor(color) {
 
 const Feed = ({ navigation }) => {
   const [userData, setUserData] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [photo, setPhoto] = useState(null);
+  const [isCameraVisible, setIsCameraVisible] = useState(false);
+
+  let cameraRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
 
   useEffect(() => {
     const currentUser = auth.currentUser;
@@ -52,9 +75,12 @@ const Feed = ({ navigation }) => {
     navigation.navigate("Profile", { updatedData: Date.now() });
   };
 
-  const getImageUrl = (profilePicture) => {
-    const timestamp = Date.now();
-    return `${profilePicture}?t=${timestamp}`;
+  const takePhoto = async () => {
+    if (cameraRef) {
+      let photo = await cameraRef.current.takePictureAsync();
+      setPhoto(photo);
+      setIsCameraVisible(false);
+    }
   };
 
   return (
@@ -77,10 +103,42 @@ const Feed = ({ navigation }) => {
               },
           ]}
         >
+          <Modal visible={isCameraVisible} transparent={true}>
+            <Camera style={styles.camera} type={type} ref={cameraRef}>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={styles.closeButton}
+                  onPress={() => setIsCameraVisible(false)}
+                >
+                  <Icon name="close" size={30} color="#fff" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.flipButton}
+                  onPress={() => {
+                    setType(
+                      type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                    );
+                  }}
+                >
+                  <Icon name="camera-reverse-outline" size={30} color="#fff" />
+                </TouchableOpacity>
+                <View style={styles.captureButtonContainer}>
+                  <TouchableOpacity
+                    style={styles.takePhotoButton}
+                    onPress={takePhoto}
+                  >
+                    <Icon name="camera-outline" size={30} color="#fff" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Camera>
+          </Modal>
           {userData && userData.profilePicture ? (
             <Image
               style={styles.profilePic}
-              source={{ uri: getImageUrl(userData.profilePicture) }}
+              source={{ uri: userData.profilePicture }}
             />
           ) : (
             <Text
@@ -94,6 +152,13 @@ const Feed = ({ navigation }) => {
           )}
         </TouchableOpacity>
       </View>
+      <TouchableOpacity
+        style={styles.openCameraButton}
+        onPress={() => setIsCameraVisible(true)}
+      >
+        <Text style={styles.buttonText}>Open Camera</Text>
+      </TouchableOpacity>
+      {photo && <Image source={{ uri: photo.uri }} style={styles.image} />}
     </View>
   );
 };
@@ -130,6 +195,61 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: COLORS.black,
     fontFamily: "Nunito-Black",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row-reverse",
+    margin: 20,
+    justifyContent: "space-between",
+  },
+
+  flipButton: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+    marginBottom: 20,
+  },
+  takePhotoButton: {
+    alignSelf: "center",
+    backgroundColor: COLORS.main,
+    padding: 20,
+    borderRadius: 90,
+  },
+  captureButtonContainer: {
+    position: "absolute",
+    bottom: 10,
+    left: 0,
+    right: 0,
+    alignItems: "center",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  openCameraButton: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 20,
   },
 });
 
