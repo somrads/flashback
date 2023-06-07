7b4f7492ad9079cc635362f5a77aabeed329bda9
somrad <somrad.sharma@student.ehb.be>
2023-06-07 15:40:52 +0200

    styling add request screen

--- frontend/components/FriendRequest.js
+++ frontend/components/FriendRequest.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import { getDatabase, ref, onValue } from "firebase/database";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { COLORS } from "../constants/colors";
+import tinycolor from "tinycolor2";
+import { acceptFriendRequest } from "../db/firebase";
+
+const darkenColor = (color) => {
+  let colorObj = tinycolor(color);
+  let { r, g, b } = colorObj.toRgb();
+
+  r = Math.floor(r / 2);
+  g = Math.floor(g / 2);
+  b = Math.floor(b / 2);
+
+  return tinycolor({ r, g, b }).toString();
+};
 
 const FriendRequest = () => {
   const [currentUser, setCurrentUser] = useState(null);
   const [friendRequests, setFriendRequests] = useState([]);
+  const [usersData, setUsersData] = useState({});
 
   useEffect(() => {
     const auth = getAuth();
@@ -34,17 +49,115 @@ const FriendRequest = () => {
 
         setFriendRequests(requests);
       });
+
+      const usersRef = ref(db, "users");
+      onValue(usersRef, (snapshot) => {
+        const data = snapshot.val();
+        setUsersData(data);
+      });
     }
   }, [currentUser]);
 
+  const acceptFriendRequest = (friendId) => {
+    // Implement the logic to accept the friend request
+  };
+
   return (
-    <View>
-      <Text>Friend Requests:</Text>
-      {friendRequests.map((request) => (
-        <Text key={request}>{request}</Text>
+    <View style={styles.container}>
+      {friendRequests.map((friendId) => (
+        <View key={friendId} style={styles.friendContainer}>
+          {usersData[friendId] && usersData[friendId].profilePicture ? (
+            <Image
+              source={{ uri: usersData[friendId].profilePicture }}
+              style={styles.profilePicture}
+            />
+          ) : (
+            <View
+              style={[
+                styles.initialsContainer,
+                { backgroundColor: usersData[friendId]?.color },
+              ]}
+            >
+              <Text
+                style={[
+                  styles.initialsText,
+                  { color: darkenColor(usersData[friendId]?.color) },
+                ]}
+              >
+                {usersData[friendId]?.firstName[0] +
+                  usersData[friendId]?.lastName[0]}
+              </Text>
+            </View>
+          )}
+          <Text style={styles.userName}>
+            {usersData[friendId]?.firstName} {usersData[friendId]?.lastName}
+          </Text>
+          <TouchableOpacity
+            style={styles.acceptButton}
+            onPress={() => acceptFriendRequest(friendId)}
+          >
+            <Text style={styles.acceptButtonText}>Accept</Text>
+          </TouchableOpacity>
+        </View>
       ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: COLORS.grayWhite,
+    fontFamily: "Nunito-Regular",
+  },
+  friendContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  profilePicture: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    marginRight: 10,
+  },
+  initialsContainer: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+  },
+  initialsText: {
+    color: COLORS.grayWhite,
+    fontSize: 20,
+    fontFamily: "Nunito-Black",
+  },
+  userName: {
+    fontSize: 17,
+    color: COLORS.grayWhite,
+    fontFamily: "Nunito-Medium",
+    marginLeft: 10,
+  },
+  acceptButton: {
+    backgroundColor: COLORS.grayBlack,
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    marginLeft: "auto",
+  },
+  acceptButtonText: {
+    color: COLORS.grayWhite,
+    fontFamily: "Nunito-Medium",
+    fontSize: 14,
+  },
+});
 
 export default FriendRequest;
